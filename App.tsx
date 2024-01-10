379df7215e32548b950514f4d074859d1cb83421
Ankur1326 <swamiankur46@gmail.com>
2024-01-10 21:01:32 +0530

    Form with state

--- App.tsx
+++ App.tsx
@@ -5,113 +5,75 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useState} from 'react';
 import {
+  Button,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  TextInput,
   View,
+  Text,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const [display, setDisplay] = useState(false);
+
+  const clearValue = () => {
+    setName('');
+    setEmail('');
+    setPassword('');
+    setDisplay(false)
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <SafeAreaView>
+      <TextInput
+        onChangeText={text => setName(text)}
+        style={styles.textStyle}
+        placeholder="Enter Your name"
+        value={name}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      <TextInput
+        onChangeText={text => setEmail(text)}
+        style={styles.textStyle}
+        placeholder="Enter email address"
+        value={email}
+      />
+      <TextInput
+        secureTextEntry={true}
+        onChangeText={text => setPassword(text)}
+        style={styles.textStyle}
+        placeholder="Enter Your password"
+        value={password}
+      />
+      <View style={styles.buttonStyle}>
+        <Button onPress={() => setDisplay(true)} color={'green'} title="Get Value" />
+      </View>
+      <Button title="clear value" onPress={() => clearValue()} />
+
+      {display && (
+        <View>
+          <Text>{name}</Text>
+          <Text>{email}</Text>
+          <Text>{password}</Text>
         </View>
-      </ScrollView>
+      )}
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  textStyle: {
+    borderWidth: 2,
+    fontSize: 20,
+    marginBottom: 5,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  buttonStyle: {
+    marginBottom: 8,
   },
 });
 
